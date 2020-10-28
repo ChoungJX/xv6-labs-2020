829a4db341c3266618726c0ab85c7fe2f701fbc0
ChoungJX <zhenglinfeng43@gmail.com>
2020-10-28 16:58:54 +0800

    finish Lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,22 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if(r_scause() == 13 || r_scause() == 15){
+    uint64 get_addr = r_stval();
+    get_addr = PGROUNDDOWN(get_addr);
+    
+    char *mem;
+    mem = kalloc();
+    if(mem == 0){
+      p->killed = 1;
+      printf("kalloc failed!");
+    }
+    memset(mem, 0, PGSIZE);
+    if(mappages(p->pagetable, get_addr, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+      kfree(mem);
+      p->killed = 1;
+      printf("mappages failed!");
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
